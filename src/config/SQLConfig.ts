06327b50b7f3365a40302428df328743ff1646de
= <hardlane2000@gmail.com>
2023-05-30 20:37:20 +0200

    committing before upload

---
+++ src/config/SQLConfig.ts
@@ -0,0 +1,15 @@
+interface SQLConfigInterface {
+    host: string;
+    user: string;
+    password: string;
+    database: string;
+}
+
+const SQLConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'imdbtop250movies'
+}
+
+export default SQLConfig;
